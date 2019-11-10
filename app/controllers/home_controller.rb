65c77a8ccafac9b1a07bdb100e829b07bea36ef9
Dan <dancorpx@gmail.com>
2019-11-10 13:11:45 +1100

    removed gem, added API

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,18 +1,36 @@
 class HomeController < ApplicationController
   def index
-    @api = StockQuote::Stock.new(api_key: 'pk_90dd9d4ea08c4c30a8ee233df365eaad')
+    @api = params[:ticker]
     if params[:ticker] == ""
       @nothing = "Please enter a symbol"
     elsif params[:ticker]
-      @stock = StockQuote::Stock.quote(params[:ticker])
-      if !@stock
+      require 'json'
+      require 'net/http'
+
+      @url = "https://cloud.iexapis.com/stable/stock/" + @api + "/quote?token=pk_90dd9d4ea08c4c30a8ee233df365eaad"
+      @uri = URI(@url)
+      @response = Net::HTTP.get_response(@uri)
+      @output = JSON.parse(@response.body)
+
+
+      @logoUrl = "https://cloud.iexapis.com/stable/stock/" + @api + "/logo?token=pk_90dd9d4ea08c4c30a8ee233df365eaad"  
+      @logoUri = URI(@logoUrl)
+      @logoResponse = Net::HTTP.get(@logoUri)
+      @logoOutput = JSON.parse(@logoResponse)
+
+      if @response.message === "OK"
+        @companyName = @output['companyName']
+        @symbol = @output['symbol']
+        @latestPrice = @output['latestPrice']
+        @changePercent = @output['changePercent']
+        @primaryExchange = @output['primaryExchange']
+        @logoUrlOutput = @logoOutput['url']
+      else
         @error = "Stock not found, enter another symbol"
       end
     end
-  
   end
 
   def about
   end
-
 end
