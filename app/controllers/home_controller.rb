dbe9ab5f3cc262b36a11e57479ff50d1bbcfd4e7
Dan <dancorpx@gmail.com>
2019-11-12 15:54:38 +1100

    error handling fixed

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -10,23 +10,26 @@ class HomeController < ApplicationController
       @url = "https://cloud.iexapis.com/stable/stock/" + @api + "/quote?token=pk_90dd9d4ea08c4c30a8ee233df365eaad"
       @uri = URI(@url)
       @response = Net::HTTP.get_response(@uri)
-      @output = JSON.parse(@response.body)
-
-
-      @logoUrl = "https://cloud.iexapis.com/stable/stock/" + @api + "/logo?token=pk_90dd9d4ea08c4c30a8ee233df365eaad"  
-      @logoUri = URI(@logoUrl)
-      @logoResponse = Net::HTTP.get(@logoUri)
-      @logoOutput = JSON.parse(@logoResponse)
-
-      if @response.message === "OK"
+      
+      # Because this API returns shit if it cant find anything, custom error handling.
+      if @response.body == "Unknown symbol"
+        @error = "Stock not found, enter another symbol"
+      else
+        @output = JSON.parse(@response.body)
+        
+        # run API to get logo
+        @logoUrl = "https://cloud.iexapis.com/stable/stock/" + @api + "/logo?token=pk_90dd9d4ea08c4c30a8ee233df365eaad"  
+        @logoUri = URI(@logoUrl)
+        @logoResponse = Net::HTTP.get(@logoUri)
+        @logoOutput = JSON.parse(@logoResponse)
+        
+        # create varibles to send to view
         @companyName = @output['companyName']
         @symbol = @output['symbol']
         @latestPrice = @output['latestPrice']
         @changePercent = @output['changePercent']
         @primaryExchange = @output['primaryExchange']
         @logoUrlOutput = @logoOutput['url']
-      else
-        @error = "Stock not found, enter another symbol"
       end
     end
   end
